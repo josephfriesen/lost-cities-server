c6daf0984beac748720c6df777ce5a268080a4c8
Joe Friesen <friesen.josephc@gmail.com>
2018-12-28 18:06:14 -0800

    Add getCurrentRoundIdFromGame query resolver.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,26 +2,6 @@ function getAllCards(parent, args, context, info) {
   return context.db.query.cards()
 }
 
-async function constructRandomizedDeckInstances(parent, args, context, info) {
-  // permuted all cards, creates an array of CardInstances, ready to be fed to a drawDeck create mutation
-  let deck = await context.db.query.cards();
-  const len = deck.length;
-  let output = [];
-  let limit;
-  let rand;
-  for (let i = 0; i < len; i++) {
-    limit = len - i;
-    rand = Math.floor(Math.random() * limit);
-    const card = deck[rand];
-    const instance = {
-      orderIndex: i,
-      card: { connect: { id: card.id }}
-    }
-    output.push(instance);
-  }
-  return output;
-}
-
 async function getPlayerHand(parent, args, context, info) {
   if (args.currentPlayer == 1) {
     player = "inPlayer1Hand";
@@ -47,37 +27,34 @@ async function getPlayerHand(parent, args, context, info) {
   return query[0].cards;
 }
 
-async function getCurrentRound(parent, args, context, info) {
-  let player;
-  let roundNum;
-  const roundQuery = await context.db.query.games({ where: { id: args.gameId } },
-  `{
-    currentPlayer
+async function getCurrentRoundIdFromGame(parent, args, context, info) {
+  const output = await context.db.query.game({where: { id: args.gameId }}, `{
+    id
     currentRound
-  }`).then(response => {
-    player = response[0].currentPlayer;
-    roundNum = response[0].currentRound;
-    return context.db.query.games({ where: { id: args.gameId } },
-    `{
-      rounds(where: { roundNumInGame: ${roundNum} }) {
-        id
-        roundNumInGame
-        player1Score
-        player1Hand { id color cardType expeditionValue }
-        player1Tableau { id color cardType expeditionValue }
-        player2Score
-        player2Hand { id color cardType expeditionValue }
-        player2Tableau { id color cardType expeditionValue }
-        discardPile { orderIndex card { id color cardType expeditionValue } }
-      }
-    }`)
-  });
-  return roundQuery[0].rounds[0];
+    rounds {
+      id
+      roundNumInGame
+    }
+  }`);
+  let id;
+  output.rounds.forEach(round => {
+    if (output.currentRound == round.roundNumInGame) {
+      console.log("HIT. RoundNumInGame: ", round.roundNumInGame);
+      id = round.id;
+    }
+  })
+  return id;
+}
+
+async function getRound(parent, args, context, info) {
+  // ARGS: roundId
+  const output = await context.db.query.rounds({where: { id: args.roundId }}, info);
+  return output[0];
 }
 
 module.exports = {
   getAllCards,
-  constructRandomizedDeckInstances,
   getPlayerHand,
-  getCurrentRound,
+  getCurrentRoundIdFromGame,
+  getRound,
 }
